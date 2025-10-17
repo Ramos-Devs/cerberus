c289a796f4514c3acdde7ac2172b30031ac84981
Ramos <andfera201@gmail.com>
2025-10-16 22:13:41 -0500

    feat: Should return an error when the requets body is empty

--- src/controllers/authenticateUserController.ts
+++ src/controllers/authenticateUserController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 
-export const resolveAuthenticateUser = async (_req: Request, res: Response): Promise<Response> => {
+export const resolveAuthenticateUser = async (req: Request, res: Response): Promise<Response> => {
+  const { body } = req;
+
+  if (!body) {
+    return res.json({ message: 'Error' });
+  }
+
   return res.json({ message: 'Success' });
 };
--- tests/integration/api/authenticateUser.test.ts
+++ tests/integration/api/authenticateUser.test.ts
@@ -1,13 +1,25 @@
 import request from 'supertest';
 import app from '../../../src/app';
 
-describe('Successful user authentication', () => {
-  it('should return a success message for valid credentils', async () => {
-    const URL_ENDPOINT = '/auth/authenticate-user';
+const URL_ENDPOINT = '/auth/authenticate-user';
 
-    const response = await request(app).post(URL_ENDPOINT);
+describe('Successful user authentication', () => {
+  it('should return user data when credentials are valid', async () => {
+    const response = await request(app)
+      .post(URL_ENDPOINT)
+      .send({ value: 'value' });
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ message: 'Success' });
   });
+});
+
+describe('Failed user authentication', () => {
+  it('should return an error when the requets body is empty', async () => {
+    const response = await request(app)
+      .post(URL_ENDPOINT);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: 'Error' });
+  })
 });
